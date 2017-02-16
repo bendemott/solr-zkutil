58a940d85e736038a326b512162ae73d2cdc3a05
Ben DeMott <ben.demott@gmail.com>
2017-02-16 18:00:37 +0000

    add classifiers

--- setup.py
+++ setup.py
@@ -49,6 +49,24 @@ setup(
         'tzlocal',         # Use your machines local timezone for dates.
         'six',
     ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
     package_data={'solrzkutil': ['bin/solr-zkutil.bat']},
     keywords = ['solr', 'zookeeper', 'cli'], # arbitrary keywords
     scripts=['bin/solr-zkutil.bat', 'bin/solr-zkutil.py', 'bin/solr-zkutil']
